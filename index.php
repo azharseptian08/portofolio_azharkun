f8a5c0a929a72e84c63a12c252501037680a077a
azharseptian08 <61535920+azharseptian08@users.noreply.github.com>
2020-04-29 09:25:41 +0000

    Create index.php
    ini index

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Submision Dicoding</title>
+	<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+	<div class="container">
+	<header>
+			<nav>
+			<div class="header-top">
+				<img src="https://cdn.worldvectorlogo.com/logos/medium-m-2.svg" class="gambar" alt="">	
+			</div>
+
+				<div class="menu-toggle">
+					<input type="checkbox">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+				<ul>
+					<li><a href="#home">Home</a></li>
+					<li><a href="#portofolio">Portofolio</a></li>
+					<li><a href="#content">About</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+
+			</nav>
+	</header>
+	<main id="home">
+		<div class="container2">
+			<div class="jumbotron"></div>
+			<div class="content" id="content">
+			<div class="article">
+				<h2 align="center">Siapa Saya?</h2>
+				<p >
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, vitae nulla voluptas in autem ratione libero officia excepturi cumque omnis, pariatur inventore, nobis. Iusto recusandae dolore, quis consectetur! Modi, illo maxime omnis, atque quaerat at, itaque suscipit error aspernatur eos voluptas praesentium ipsa, nostrum numquam odio asperiores laboriosam molestias. Praesentium!
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto vel odio perferendis incidunt. Nostrum sunt aliquid minus illo possimus, ullam debitis rerum nisi delectus consequuntur aliquam numquam voluptatum? Beatae, praesentium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur doloribus, neque impedit, reiciendis dolor atque dignissimos in itaque. Itaque quam impedit earum. Suscipit minima nisi deserunt placeat doloremque unde illum ipsum ad aspernatur quis laudantium hic sunt rem nobis a perferendis, voluptas, accusantium veritatis? Voluptas illum, natus provident error veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius impedit, eveniet ad praesentium placeat, 
+				</p>
+			</div>
+			<div class="article2"  id="portofolio">
+				<h3 align="center">Pendidikan</h3>
+				<ul>
+					<li>
+						<h4>SDN PANDEGLANG 6</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius delectus ullam,<br> soluta quibusdam! <br>
+							<b>Tahun</b> - 6 tahun [2010 sampai 2016] <br>
+							<b>Lokasi</b> - Kab.Pandeglang, Banten.
+						</p>
+					</li>
+					<li>
+						<h4>SMPN 1 PANDEGLANG</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius delectus ullam, <br> soluta quibusdam! <br>
+							<b>Tahun</b> - 3 tahun [2016 sampai 2019] <br>
+							<b>Lokasi</b> - Kab.Pandeglang, Banten.
+						</p>
+					</li>
+					<li>
+						<h4>SMKN 1 PANDEGLANG</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius delectus ullam, <br> soluta quibusdam! <br>
+							<b>Tahun</b> - [2019 sampai Sekarang] <br>
+							<b>Lokasi</b> - Kab.Pandeglang, Banten.
+						</p>
+					</li>
+				</ul>
+			</div>
+			</div>
+			<aside>
+				<div class="aduh">
+					<img src="assets/img/SAVE.jpeg" >
+				</div>
+				<div class="nama">
+						<h2>Septian Azhar</h2>
+						<p>Pelajar</p>
+				</div>
+			</aside>
+		</div>
+	</main>
+	<footer id="contact">
+		<div class="inner-footer">
+
+			<div class="footer-items">
+				<h1>Portofolio and Article</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, excepturi!jkhkhjkhjk
+				</p>
+			</div>
+			<div class="footer-items">
+				<h2>Quick Links</h2>
+				<div class="border"></div>
+				<ul>
+					<a href=""><li>Home</li></a>
+					<a href=""><li>Portofolio</li></a>
+					<a href=""><li>About</li></a>
+					<a href=""><li>Contact us</li></a>
+				</ul>
+			</div>
+			<div class="footer-items">
+				<h2>Tutorials</h2>
+				<div class="border"></div>
+				<ul>
+					<a href=""><li>HTML</li></a>
+					<a href=""><li>CSS</li></a>
+					<a href=""><li>Javascript</li></a>
+					<a href=""><li>PHP</li></a>
+				</ul>
+			</div>
+			<div class="footer-items">
+				<h2>Contact Us</h2>
+				<div class="border"></div>
+				<ul>
+					<li><img src="assets/img/icon/map.svg">Kp. Cikondang</li>
+					<li><img src="assets/img/icon/envelope.svg">azharseptian611@gmail.com</li>
+					<li><img src="assets/img/icon/phone.svg">089630506862</li>
+				</ul>
+				<div class="social-media">
+					<li><a href=""><img src="assets/img/icon/facebook.svg"></a>
+					<a href=""><img src="assets/img/icon/twitter.svg"></a>
+					<a href=""><img src="assets/img/icon/youtube.svg"></a>
+					</li>
+				</div>
+			</div>
+
+		</div>
+		<div class="footer-bottom">
+			Copyrights &copy; Portofolio and Article 2020.s
+		</div>
+	</footer>
+</div>
+
+	<script src="assets/script.js"></script>
+</body>
+</html>
